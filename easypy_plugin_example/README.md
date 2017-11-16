a378c967ee6fe72fd69ba8a41cc7de9fb3f0d5ce
Wei Chen <weiche3@cisco.com>
2017-11-16 14:28:17 -0500

    initial commit of easypy plugin example

---
+++ easypy_plugin_example/README.md
@@ -0,0 +1,36 @@
+# Easypy Plugin Example
+
+Easypy Plugins offers optional functionalities that may be added to easypy. Each
+plugin must be configured first via a configuration YAML file before they can be
+loaded, instantiated and run at various stages of execution.
+
+This directiory contains a whole example of how to write an Easypy plugin
+package where:
+
+- the plugin comes in a form standard python package
+- is auto-discovered by Easypy upon installation and configuration
+- enables Easypy to run at various stages of execution defined by this plugin.
+
+For more information on pyATS and Easypy, visit:
+https://developer.cisco.com/site/pyats/
+
+## Using This Plugin
+
+To use this plugin, clone this folder into your local pyATS environment, and:
+
+```
+$ cd easypy_plugin_example
+
+# install this package permanently into this virtual environment
+$ python setup.py install
+
+# or, develop this package (where changes in src/ folder refect in your environment immediately)
+$ python setup.py develop
+
+# enable this plugin globally (for all runs in this pyATS instance) at this virtual environment
+$ cp easypy_config.yaml
+
+# or, enable the plugin locally (for this run only) by passing in a config YAML via a command-line argument called -configuration.
+$ easypy easypy_job.py -configuration easypy_config.yaml
+
+```
---
+++ easypy_plugin_example/setup.py
@@ -0,0 +1,114 @@
+#! /bin/env python
+
+'''
+Easypy Plugin Example Setup File
+--------------------------------
+
+This is a sample setup.py file that enables developers to build their own
+Easypy plugin to enable support for enhancing their easypy at various stages of
+execution.
+
+setup.py is a standard Python packaging mechanism. For more information on how
+to write this file, please refer to:
+    setuptools: https://setuptools.readthedocs.io/en/latest/setuptools.html
+    distutils: https://docs.python.org/3/distutils/setupscript.html
+
+The only part to be aware of when writing a setup.py file for Easypy plugins,
+is to ensure you use indicate the correct entrypoint argument:
+
+    setup(
+        # ...
+        entry_points = {'easypy.plugins': ['<plugin_name> = <module_name>']},
+        # ...
+    )
+
+where <plugin_name> should be replaced with with your plugin name (allowed
+character set A-Za-z0-9_), and <module_name> replaced with the module where your
+plugin is implemented within this package. Note that this also supports a list
+of plugins per package.
+'''
+
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    'convenience function to read a file into text'
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+
+    # name of your plugin, this will become the package name
+    name = 'easypy-plugin-example',
+
+    # package version string
+    version = '1.0',
+
+    # descriptions
+    description = 'Easypy Plugin Example',
+
+    # use README.md as long description
+    long_description = read('README.md'),
+
+    # the project's main homepage.
+    url = 'https://developer.cisco.com/site/pyats/',
+
+    # author details
+    author = 'Cisco Systems',
+    author_email = 'pyats-support-ext@cisco.com',
+
+    # project licensing
+    license = 'Apache 2.0',
+
+    # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    # project keywords
+    keywords = 'pyats easypy plugin connection',
+
+    # project packages
+    packages = find_packages(where = 'src'),
+
+    # project directory
+    package_dir = {
+        '': 'src',
+    },
+
+    # additional package data files that goes into the package itself
+    package_data = {},
+
+    # console entry point (pay attention here)
+    # as an example, we are implementing a platform called "example"
+    entry_points = {'easypy.plugins': ['example = easypy_plugin_example']},
+
+    # package dependencies
+    install_requires =  ['setuptools', 'ats.easypy'],
+
+    # any additional groups of dependencies.
+    # install using: $ pip install -e .[dev]
+    extras_require = {},
+
+    # any data files placed outside this package.
+    data_files = [],
+
+    # custom commands for setup.py
+    cmdclass = {},
+
+    # non zip-safe (never tested it)
+    zip_safe = False,
+)
